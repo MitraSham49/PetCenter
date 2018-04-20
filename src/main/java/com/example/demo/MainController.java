c4c979a1f72ab20f91c7971208da7e28072ab79a
MitraSham49 <mairmeds@gmail.com>
2018-04-20 12:03:21 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/MainController.java
@@ -0,0 +1,54 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    @Autowired
+    PetRepository petRepo;
+
+     @RequestMapping("/")
+    public String showIndex (Model model){
+         model.addAttribute("inventory", petRepo.findAll());
+       return"index"  ;
+     }
+
+     @RequestMapping("/add")
+    public  String addPet(Model model){
+         model.addAttribute( "aPet", new Pet());
+           return"addPet" ;
+     }
+
+     @RequestMapping("/savepet")
+    public  String savePet(@ModelAttribute ("aPet") Pet toSave, BindingResult result){
+    if (result.hasErrors()){
+        return"addpet";
+    }
+     petRepo.save (toSave);
+    return "redirect:/";
+     }
+    @RequestMapping("/changestatus/{id}")
+
+    public String borrowReturn(@PathVariable("id") long id) {
+         Pet thisPet = petRepo.findById(id).get();
+         thisPet.setLost(!thisPet.isLost());
+         petRepo.save(thisPet);
+         return "redirect:/";
+    }
+    @RequestMapping("/update/{id}")
+    public String updatePet(@PathVariable ("id") long id, Model model) {
+        model.addAttribute("aPet", petRepo.findById(id).get());
+        return "addpet";
+    }
+    @RequestMapping("/detail/{id}")
+    public String showList(@PathVariable("id")long id, Model model){
+        model.addAttribute("aPet", petRepo.findById(id).get());
+         return"redirect:/";
+    }
+}
---
+++ src/main/java/com/example/demo/Pet.java
@@ -0,0 +1,68 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Pet {
+     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+     private long id;
+      @NotNull
+    private String  type;
+    private String  name;
+    private String  description;
+    private String image;
+    private boolean lost;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public boolean isLost() {
+        return lost;
+    }
+
+    public void setLost(boolean lost) {
+        this.lost = lost;
+    }
+}
